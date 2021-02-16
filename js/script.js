e937b70639009d974069b8b3d5fb8c262c530f21
meritarl <merita.rintalaulaja@gmail.com>
2021-02-16 18:52:18 +0200

    added menu toggle

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log("Hello, welcome to my portfolio!")
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
